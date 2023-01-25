f06c0922b19e0741adf1098c57e2de1ef4c5a22a
auwerk <noisefront@yandex.ru>
2023-01-25 20:16:54 +0300

    Kubernetes host constant in K6 users srcipt

--- k6-scripts/users.js
+++ k6-scripts/users.js
@@ -2,6 +2,8 @@ import { check, fail, sleep } from 'k6';
 import http from 'k6/http';
 import exec from 'k6/execution';
 
+const KUBE_HOST = "arch.homework:30043";
+
 function initUserData(userNum) {
 	return {
         username: `user${userNum}`,
@@ -21,7 +23,7 @@ function createUser(userNum) {
         },
     };
 
-    const res = http.post('http://arch.homework/user/', JSON.stringify(user), params);
+    const res = http.post(`http://${KUBE_HOST}/user/`, JSON.stringify(user), params);
 
     const checkResponse = check(res, { 'createUser.status is 200': (r) => r.status == 200 });
     if (!checkResponse) {
@@ -40,22 +42,22 @@ function duplicateUser(userNum) {
         },
     };
 
-    const res = http.post('http://arch.homework/user/', JSON.stringify(user), params);
+    const res = http.post(`http://${KUBE_HOST}/user/`, JSON.stringify(user), params);
 	check(res, { 'duplicateUser.status is 500': (r) => r.status == 500 });
 }
 
 function deleteUser(userId) {
-    const res = http.del(`http://arch.homework/user/${userId}`);
+    const res = http.del(`http://${KUBE_HOST}/user/${userId}`);
     check(res, { 'deleteUser.status is 204': (r) => r.status == 204 });
 }
 
 function getUserById(userId) {
-	const res = http.get(`http://arch.homework/user/${userId}`);
+	const res = http.get(`http://${KUBE_HOST}/user/${userId}`);
 	check(res, { 'getUserById.status is 200': (r) => r.status == 200 });
 }
 
 function getUsers() {
-	const res = http.get('http://arch.homework/user/');
+	const res = http.get(`http://${KUBE_HOST}/user/`);
 	check(res, { 'getUsers.status is 200': (r) => r.status == 200 });
 }
 
